87104e3ae91d3bc043556de6a5eacaf5c274f6df
Esraa Abduallah <esraahisham753@gmail.com>
2026-06-05 12:20:58 +0300

    Feat: Getting started with Frontend Application

---
+++ src/app/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+  imports: [RouterLink],
+})
+export class Header {}
