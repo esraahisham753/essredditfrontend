df8880e69cb7251a3c13fa67bdfcff79e69de7b8
Esraa Abduallah <esraahisham753@gmail.com>
2026-06-20 10:37:13 +0300

    Feat: Implement view subreddit layout with responsive design and sidebar integration

--- src/app/subreddit/view-subreddit/view-subreddit.ts
+++ src/app/subreddit/view-subreddit/view-subreddit.ts
@@ -3,10 +3,12 @@ import { PostModel } from '../../shared/PostType';
 import { Observable } from 'rxjs';
 import { Post } from '../../shared/post';
 import { AsyncPipe } from '@angular/common';
+import { Sidebar } from '../../shared/sidebar/sidebar';
+import { PostTile } from '../../shared/post-tile/post-tile';
 
 @Component({
   selector: 'app-view-subreddit',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, Sidebar, PostTile],
   templateUrl: './view-subreddit.html',
   styleUrl: './view-subreddit.css',
 })
